b0c5489a669db36cd2b5aa14a720b9994ad3a7d0
Maor Leger <maor.leger@gmail.com>
2018-02-18 12:21:18 -0700

    clean up dot files

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.1"]
+{:user {:plugins [[cider/cider-nrepl "0.16.0"]
                   [lein-auto  "0.1.3"]
+                  [lein-cljfmt "0.5.7"]
                   [venantius/ultra  "0.5.2"]]}}
