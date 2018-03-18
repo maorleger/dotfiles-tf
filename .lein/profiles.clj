ee97ba4996e56bed2567deed60a56d9c63816514
Maor Leger <maor.leger@gmail.com>
2018-03-18 17:12:38 -0600

    move cljfmt to dependencies

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[cider/cider-nrepl "0.16.0"]
                   [lein-auto  "0.1.3"]
-                  [lein-cljfmt "0.5.7"]
-                  [venantius/ultra  "0.5.2"]]}}
+                  [lein-cljfmt "0.5.1"]
+                  [venantius/ultra  "0.5.2"]]
+        :dependencies [[cljfmt  "0.5.1"]]}}
